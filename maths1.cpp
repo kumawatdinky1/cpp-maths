4851999e1cd71e1679f541d137b7d439f2e5ceee
Dinky kumawat <97829597+kumawatdinky1@users.noreply.github.com>
2023-05-27 16:30:23 +0530

    Update maths1.cpp

--- maths1.cpp
+++ maths1.cpp
@@ -1,2 +1,20 @@
 #include<iostream>
 using namespace std;
+int count(int n){
+    //write your code here
+    int count = 0;
+    while (n > 0) {
+      
+      int lastdigit = n % 10;
+      count++;
+      n = n / 10;
+    }
+    return count;
+}
+
+int main()
+{
+  int n;
+  cin>>n;
+  count(n);
+}
